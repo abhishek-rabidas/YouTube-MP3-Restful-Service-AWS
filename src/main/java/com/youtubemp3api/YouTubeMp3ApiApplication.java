41f619074c59e38d1a2769fa965bec5a8fe8c93d
viiiraj07 <avisec07@gmail.com>
2021-09-09 00:25:43 +0530

    initialization

---
+++ src/main/java/com/youtubemp3api/YouTubeMp3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.youtubemp3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeMp3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeMp3ApiApplication.class, args);
+	}
+
+}
